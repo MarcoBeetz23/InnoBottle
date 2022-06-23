2bf4604691a5bfc1149396f7d6e63732bab8f572
JK <julia.kadur@tum.de>
2022-06-23 16:03:26 +0200

    minor changes

--- app/src/main/java/com/example/innobottle/View/MainActivity.java
+++ app/src/main/java/com/example/innobottle/View/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.innobottle.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.innobottle.Presenter.MainContract;
 import com.example.innobottle.Presenter.MainPresenter;
